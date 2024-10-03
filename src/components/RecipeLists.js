79b04eb46cb3ec34c8310d402b7f39eadaa3227b
gouravkalsi <kalsigourav86280@gmail.com>
2024-10-03 13:48:42 +0530

    initial2 deploy

--- src/components/RecipeLists.js
+++ src/components/RecipeLists.js
@@ -1,54 +1,52 @@
-import React, { useEffect, useState } from 'react';
-import { fetchData } from '../service';
- // Adjust the path accordingly
+import React,{useEffect,useState} from 'react'
+import {BsSearch} from 'react-icons/bs'
+import {fetchData} from "../service"
 
+function RecipeLists(props) {
+    const [searchedTearm, setSearchedTearm] = useState('')
+    const [query,setQuery] = useState('pasta')
+    const [data,setData] = useState('');
 
-const RecipeLists = ({ query }) => {
-    const [recipes, setRecipes] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+   const searchrecipe = (searchQuery) => {
+    fetchData(searchQuery).then((response)=> {
+        setData(response)
+        props.setLoader(false)
+    })
+   }
 
-    useEffect(() => {
-        const fetchRecipes = async () => {
-            setLoading(true);
-            setError(null); // Reset error state
-
-            const response = await fetchData(query);
-
-            // Check if response is valid
-            if (response && response.hits) {
-                setRecipes(response.hits);
-            } else {
-                setError('No recipes found or an error occurred.');
-            }
-            setLoading(false);
-        };
-
-        fetchRecipes();
-    }, [query]);
-
-    if (loading) {
-        return <div>Loading...</div>;
-    }
-
-    if (error) {
-        return <div>Error: {error}</div>;
-    }
-
-    return (
-        <div>
-            {recipes.length > 0 ? (
-                recipes.map((recipe, index) => (
-                    <div key={index}>
-                        <h2>{recipe.recipe.label}</h2>
-                        {/* Other recipe details */}
+    useEffect(()=>{
+        fetchData(query).then((response)=> {
+            setData(response)
+            props.setLoader(false)
+        })
+    },[])
+  return (
+    <div className='container'>
+        <div className='heading-line'>
+            <strong>Search Recipes</strong>
+            <div className='input-wrapper' >
+                <input 
+                    onChange={(e)=> setSearchedTearm(e.target.value)} 
+                    value={searchedTearm} 
+                    type="text" 
+                    placeholder='Search you recipe' />
+                <button onClick={()=> (searchrecipe(searchedTearm),props.setLoader(true))}><BsSearch /></button>
+            </div> 
+        </div>
+        <div className='flexbox'>
+            {
+                data && data.hits.map((item,index)=> (
+                    <div key={index} className='flexItem'>
+                        <div className='img-wrapper'>
+                            <img src={item.recipe.image} alt={item.recipe.label} />
+                        </div>
+                        <p>{item.recipe.label}</p>
                     </div>
                 ))
-            ) : (
-                <div>No recipes available.</div>
-            )}
+            }
         </div>
-    );
-};
+    </div>
+  )
+}
 
-export default RecipeLists;
+export default RecipeLists
